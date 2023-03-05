bd4c388551a8b681de93e8a19d8e0a64326738e2
shriishvaram77 <126367552+shriishvaram77@users.noreply.github.com>
2023-03-05 00:20:39 -0500

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,4 +1,7 @@
-def get_capital_letters():
+def get_allcapital_letters():
+    """
+    Returns the input phraze with all letters capital
+    """
     user_input = input()
     return user_input.upper()
 
