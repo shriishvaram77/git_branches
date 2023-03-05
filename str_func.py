b72644a6f0386410e5e7174d5ca509745f292269
Igor G <i.gorbatenco@gmail.com>
2023-03-05 17:22:58 -0500

    1. Added the second function that gets the phrase and returns it with first letter of each word to be capital 2. Added docstrings to the first function on the file

--- str_func.py
+++ str_func.py
@@ -1,6 +1,20 @@
 def get_capital_letters():
+    """
+    Returns the input phrase with all letters to be capital
+    """
     user_input = input()
     return user_input.upper()
 
 
 print(get_capital_letters())
+
+
+def get_first_letters_capital():
+    """
+    Returns the input phrase with first letters of each word becoming capital
+    """
+    user_input = input()
+    return user_input.title()
+
+
+print(get_first_letters_capital())
