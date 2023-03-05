cf71abc754d6190117e24937a473b26daff74b0c
shriishvaram77 <126367552+shriishvaram77@users.noreply.github.com>
2023-03-05 17:14:54 -0500

    Revert "Develop"

--- str_func.py
+++
@@ -1,9 +0,0 @@
-def get_allcapital_letters():
-    """
-    Returns the input phraze with all letters capital
-    """
-    user_input = input()
-    return user_input.upper()
-
-
-print(get_capital_letters())
